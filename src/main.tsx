97751152bb38d2a1999b78e79271fecd70987577
Kermitroid <kb1460840@gmail.com>
2025-04-28 12:12:42 -0400

    posthog

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: "https://us.i.posthog.com",
+          debug: import.meta.env.MODE === "development",
+        }}
+      >
+        <App />
+      </PostHogProvider>
     </BrowserRouter>
   </StrictMode>
 );
